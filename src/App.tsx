c893dc1a2f1590a99b5c9cf7f8eb85d76729916c
HiImConan <mindol0320@naver.com>
2022-08-11 19:34:11 +0900

    typescript: optional props

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="blue" />
+      <Circle bgColor="tomato" text="hello" />
+    </div>
+  );
+}
+
+export default App;
