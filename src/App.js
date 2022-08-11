c340cccbcc4476a175fc78da50e10d48560f0060
HiImConan <mindol0320@naver.com>
2022-08-11 10:10:12 +0900

    pseudo-selector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minlength: 10 })`
-  background-color: tomato;
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  } 50% {
+    
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
+`; // selector
 
 const App = () => {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper as="header">
+      <Box>
+        <Emoji as="p">☺️</Emoji>
+      </Box>
+    </Wrapper>
   );
 };
 
