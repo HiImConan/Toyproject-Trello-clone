15460128bbf0c00b2d9fee8b6ffe087f34fe8a42
HiImConan <mindol0320@naver.com>
2022-08-17 01:51:24 +0900

    react-hook-form: validate

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -31,19 +31,109 @@ import { useForm } from "react-hook-form";
 //   );
 // };
 
+type IFormData = {
+  errors: {
+    email: {
+      message: string;
+    };
+  };
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password2: string;
+  extraError?: string;
+};
+
 const ToDoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IFormData) => {
+    if (data.password !== data.password2) {
+      setError(
+        "password2",
+        { message: "Passwords are not the same." },
+        { shouldFocus: true }
+      );
+    }
+    //setError("extraError", { message: "Server offline." });
+  };
+  console.log(errors);
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("First Name")} placeholder="FirstName" />
-        <input {...register("Last Name")} placeholder="Last Name" />
-        <input {...register("Username")} placeholder="Username" />
-        <input {...register("Password")} placeholder="Password" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "Need to fill this container",
+            validate: {
+              noNick: (value) => !value.includes("nick") || "no nick allowed",
+              includeMingyu: (value) =>
+                value.includes("mingyu") || "no mingyu included",
+              // value validate에서 단어 포함 유무를 물을 때 순서: 불허 단어 -> 필수 포함 단어 순
+            },
+          })}
+          placeholder="FirstName"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", {
+            required: "Need to fill this container",
+            minLength: 3,
+          })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", {
+            required: "Need to fill this container",
+            minLength: 5,
+          })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: { value: 5, message: "Your password is too short" },
+          })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password2", {
+            required: "Password2 is required",
+            minLength: { value: 5, message: "Your password is too short" },
+          })}
+          placeholder="Password2"
+        />
+        <span>{errors?.password2?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
