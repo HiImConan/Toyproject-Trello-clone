a7cd1a1c86c5e2e190509dede6d2399c84bb175c
HiImConan <mindol0320@naver.com>
2022-08-28 23:25:52 +0900

    Recoil: selector

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
