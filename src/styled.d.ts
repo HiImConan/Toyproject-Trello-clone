c116a32469e9d7c3edd9dc93351ac6c68c7ce198
HiImConan <mindol0320@naver.com>
2022-11-01 13:12:20 +0900

    Trello Challenge-Refactor: 1. light/darktheme 설정

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,7 +3,19 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
-    boardColor: string;
+    textColor: string;
+    secondaryTextColor: string;
+    accentColor: string;
+    accentFadedColor: string;
+
     cardColor: string;
+    activeCardColor: string;
+
+    boardColor: string;
+    buttonColor: string;
+    hoverButtonColor: string;
+    hoverButtonOverlayColor: string;
+    glassColor: string;
+    scrollBarColor: string;
   }
 }
