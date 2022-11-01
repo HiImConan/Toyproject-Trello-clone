e8870adbc5dbe0c81e2f8e6093e4786cc8e7a05d
HiImConan <mindol0320@naver.com>
2022-11-01 13:13:30 +0900

    Trello Challenge-Refactor: 2. recoil-persist 인스톨

--- src/atom.ts
+++ src/atom.ts
@@ -1,11 +1,14 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export interface ITodo {
   id: number;
   text: string;
 }
 
-interface IToDoState {
+export interface IToDoState {
   // consider scalability
   [key: string]: ITodo[];
 }
@@ -17,4 +20,11 @@ export const toDoState = atom<IToDoState>({
     Doing: [],
     Done: [],
   },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const BoardState = atom<string[]>({
+  key: "boards",
+  default: ["To Do", "Doing", "Done"],
+  effects_UNSTABLE: [persistAtom],
 });
