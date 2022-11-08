2b71de0d26d669b0fef98636e184f2589fc7cb9d
HiImConan <mindol0320@naver.com>
2022-11-08 13:18:05 +0900

    Trello Challenge-Refactor: 9. 로컬스토리지 atom 저장 로직 완성

--- src/atom.ts
+++ src/atom.ts
@@ -11,6 +11,59 @@ export interface IBoard {
   toDos: IToDo[];
 }
 
+const instanceOfToDo = (object: any): object is IToDo => {
+  return (
+    object.constructor === Object &&
+    "id" in object &&
+    "text" in object &&
+    typeof (object as IToDo).id === "number" &&
+    typeof (object as IToDo).text === "string"
+  );
+};
+
+const instanceOfBoard = (object: any): object is IBoard[] => {
+  return (
+    object.constructor === Object &&
+    "id" in object &&
+    "title" in object &&
+    "toDos" in object &&
+    typeof (object as IBoard).id === "number" &&
+    typeof (object as IBoard).title === "string" &&
+    (object as IBoard).toDos.every((toDo) => instanceOfToDo(toDo))
+  );
+};
+
+const instanceOfBoards = (object: any): object is IBoard[] => {
+  return (
+    Array.isArray(object) &&
+    (object as IBoard[]).every((board) => instanceOfBoard(board))
+  );
+};
+
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const data = localStorage.getItem(key);
+
+    if (data !== null && data !== undefined) {
+      const json = (raw: string) => {
+        try {
+          return JSON.parse(raw);
+        } catch (error) {
+          return false;
+        }
+      };
+
+      if (json(data) && instanceOfBoards(json(data))) {
+        setSelf(json(data));
+      }
+    }
+
+    onSet((newValue: IBoard[]) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const toDosState = atom<IBoard[]>({
   key: "toDos",
   default: [
@@ -36,7 +89,7 @@ export const toDosState = atom<IBoard[]>({
       ],
     },
   ],
-  // 로컬스토리지 저장 로직 추가 예정 effects: [localStorageEffect('trello-clone-todos')]
+  effects: [localStorageEffect("trello-clone-todos")],
 });
 
 export const isLightState = atom<boolean>({
